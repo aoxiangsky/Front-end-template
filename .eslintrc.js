abb8db9889fcd7a543af6f18165ca4903f09fef1
敖翔 <henry.ao@marketingforce.com>
2022-06-09 18:19:51 +0800

    feat: init

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+        es2021: true,
+    },
+    extends: ["eslint:recommended", "prettier"],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    rules: {},
+};
